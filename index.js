6bfba638eed123b10a6704fa8f4847053380a0d9
leeight <leeight@gmail.com>
2014-02-12 22:01:48 +0800

    init repos

---
+++ index.js
@@ -0,0 +1,47 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * index.js ~ 2014/02/12 21:25:18
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ *  
+ **/
+var fs = require('fs');
+var edp = require( 'edp-core' );
+
+exports.start = function (args, opts) {
+    // 如果output目录存在，检查css引用的img是否正确
+    require( './lib/check-output.js' )( args, opts );
+
+    // 检查项目中是否存在相同的文件
+    // TODO
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100: */
